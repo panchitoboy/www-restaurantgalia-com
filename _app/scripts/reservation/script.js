917cb52c30612b9a7fbe0d10a25ad0442aa33a71
Jaime Martinez <panchitoboy@gmail.com>
2019-04-05 22:22:18 +0200

    update mail

--- _app/scripts/reservation/script.js
+++ _app/scripts/reservation/script.js
@@ -1,85 +1,68 @@
-$('.datepicker').datepicker({
-    startDate: '0',
-    autoclose: true,
-    beforeShowDay: function (day) {
-        var start = new Date('2019-02-25'); 
-        var end = new Date('2019-03-12'); 
-         if(day > start && end > day){
-             return false;
-         }
-
-        if (day.getDay() === 1) {
-            return false;
-        } else {
-            return true;
-        }
+$(".datepicker").datepicker({
+  startDate: "0",
+  autoclose: true,
+  beforeShowDay: function(day) {
+    var start = new Date("2019-02-25");
+    var end = new Date("2019-03-12");
+    if (day > start && end > day) {
+      return false;
     }
+
+    if (day.getDay() === 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
 });
 
-$('.clockpicker').clockpicker({
-    placement: 'bottom',
-    align: 'left',
-    autoclose: true,
-    'default': 'now'
+$(".clockpicker").clockpicker({
+  placement: "bottom",
+  align: "left",
+  autoclose: true,
+  default: "now"
 });
-
 
 var options = {
-    rules: {
-        name: {
-            minlength: 2,
-            maxlength: 50,
-            required: true
-        },
-        date: {
-            date: true,
-            required: true
-        },
-        email: {
-            email: true,
-            required: true
-        },
-        time: {
-            required: true
-        },
-        phone: {
-            number: true,
-            required: true
-        },
-        quantity: {
-            number: true,
-            min: 1,
-            max: 20,
-            required: true
-        }
+  rules: {
+    name: {
+      minlength: 2,
+      maxlength: 50,
+      required: true
     },
-    errorElement: "span",
-    errorClass: "error text-danger",
-    errorPlacement: function (error, element) {
-        var name = $(element).attr("name");
-        error.appendTo($("#" + name + "_validate"));
+    date: {
+      date: true,
+      required: true
     },
-    submitHandler: function (form, event) {
-        event.preventDefault();
-        $("#submitButton").hide();
-        var data = $('form').serializeArray().reduce(function (result, value) {
-            result[value.name] = value.value;
-            return result;
-        }, {});
-
-        $.ajax({
-            url: "https://formspree.io/restaurantgalia@hotmail.com",
-            method: "POST",
-            data: data,
-            dataType: "json",
-            error: function (request, error) {
-                $("#responseMessage").text('Nous vous informons que un erreur est survenu au moment de traiter votre reservation. Veuillez nous appeller par telephone pour faire votre reservation.');
-            },
-            success: function () {
-                $("#responseMessage").text('Nous vous informons que votre reservation a été pris en compte.');
-            }
-        });
+    email: {
+      email: true,
+      required: true
+    },
+    time: {
+      required: true
+    },
+    phone: {
+      number: true,
+      required: true
+    },
+    quantity: {
+      number: true,
+      min: 1,
+      max: 20,
+      required: true
     }
+  },
+  errorElement: "span",
+  errorClass: "error text-danger",
+  errorPlacement: function(error, element) {
+    var name = $(element).attr("name");
+    error.appendTo($("#" + name + "_validate"));
+  },
+  submitHandler: function(form, event) {
+    event.preventDefault();
+    $("#submitButton").hide();
+    form.submit();
+  }
 };
 
-$('#reservation-form').validate(options);
+$("#reservation-form").validate(options);
